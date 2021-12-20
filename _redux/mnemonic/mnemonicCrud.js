db1ccf3eb47e2ca56396d22ba645a0e604213aa0
Justin <justin@nxt.work>
2021-12-20 21:22:58 +0800

    - update

--- _redux/mnemonic/mnemonicCrud.js
+++ _redux/mnemonic/mnemonicCrud.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// export const MNEMONIC_API_URL =
-('https://itp-mnemonic.herokuapp.com/api/lexicon');
+export const MNEMONIC_API_URL =
+  'https://itp-mnemonic.herokuapp.com/api/lexicon';
 // export const MNEMONIC_API_URL = 'http://192.168.1.150:8000/api/lexicon';
 
 export const fetchWordsByLetter = (uid, letter) => {
